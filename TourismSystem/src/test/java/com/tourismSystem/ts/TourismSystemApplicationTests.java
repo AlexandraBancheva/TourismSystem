1e51d836fadd9717683ac9f1abd2973faeee207c
AlexandraBancheva <sandriche90@gmail.com>
2023-02-10 13:42:33 +0200

    Initial create

---
+++ TourismSystem/src/test/java/com/tourismSystem/ts/TourismSystemApplicationTests.java
@@ -0,0 +1,13 @@
+package com.tourismSystem.ts;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TourismSystemApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
