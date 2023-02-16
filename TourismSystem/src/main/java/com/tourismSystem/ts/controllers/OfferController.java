9256549031b3c3b549c85650905352533fce23eb
AlexandraBancheva <sandriche90@gmail.com>
2023-02-16 20:23:13 +0200

    Added new method in OfferController.

--- TourismSystem/src/main/java/com/tourismSystem/ts/controllers/OfferController.java
+++ TourismSystem/src/main/java/com/tourismSystem/ts/controllers/OfferController.java
@@ -4,16 +4,22 @@ import com.tourismSystem.ts.entities.Category;
 import com.tourismSystem.ts.entities.Offer;
 import com.tourismSystem.ts.entities.Transport;
 import com.tourismSystem.ts.services.CategoryService;
+import com.tourismSystem.ts.services.ImageService;
 import com.tourismSystem.ts.services.OfferService;
 import com.tourismSystem.ts.services.TransportService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.multipart.MultipartFile;
 import org.springframework.web.servlet.ModelAndView;
 
+import java.io.IOException;
 import java.util.ArrayList;
 import java.util.List;
 
@@ -22,13 +28,16 @@ public class OfferController {
     private OfferService offerService;
     private CategoryService categoryService;
     private TransportService transportService;
+    private ImageService imageService;
 
     public OfferController(OfferService offerService,
                            CategoryService categoryService,
-                           TransportService transportService) {
+                           TransportService transportService,
+                           ImageService imageService) {
         this.offerService = offerService;
         this.categoryService = categoryService;
         this.transportService = transportService;
+        this.imageService = imageService;
     }
 
     @ModelAttribute("categories")
@@ -64,5 +73,11 @@ public class OfferController {
     public String saveOffer(@ModelAttribute Offer offer){
         offerService.saveOffer(offer);
         return "redirect:/offers";
+    }
+
+    @PostMapping("/upload")
+    public ResponseEntity<?> uploadImageToFileSystem(@RequestParam("image")MultipartFile file) throws IOException {
+        String uploadImage = imageService.uploadImageToFileSystem(file);
+        return ResponseEntity.status(HttpStatus.OK).body(uploadImage);
     }
 }
